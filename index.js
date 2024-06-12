48a80663d6daa2cb087eb6a51e7bbd5248729959
FebrianAditya <febrian.aditya@student.upi.edu>
2024-06-12 10:25:45 +0700

    add 3 practice

---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+
+Soal Latihan: Menentukan Nilai Siswa
+
+Buatlah fungsi yang dapat menentukan kategori Nilai Siswa berdasarkan score sebagai berikut:
+1. Nilai 0-1: "D"
+2. Nilai 2-5: "C"
+3. Nilai 6-8: "B"
+4. Nilai 9-10: "A"
+5. Nilai > 10 dan Nilai < 0: "Invalid Score"
+
+Kalian diminta membuat fungsi kategoriScore(nilai) yang menerima parameter:
+1. nilai (number): score hasil ujian siswa.
+
+Fungsi ini harus mengembalikan kategori nilai berdasarkan rentang score yang diberikan.
+
+*/
+
+function kategoriScore(nilai) {
+
+}
+
+console.log(kategoriScore(8));
+// // Output: B
+console.log(kategoriScore(15));
+// // Output: Invalid Score
+console.log(kategoriScore(1));
+// // Output: D
+console.log(kategoriScore(2));
+// Output: C
+
+
+
+
+
+/**
+ * 
+ * Soal latihan: Menentukan Positif atau Negatif dari angka
+ * 
+ * Buatlah sebuah fungsi checkNumber yang menerima satu parameter num. 
+ * Ketentuan:
+ *  1. Fungsi tersebut harus mengembalikan "Positif" jika num lebih besar dari 0, 
+ *  2. "Negatif" jika num lebih kecil dari 0, 
+ *  3. dan "Nol" jika num sama dengan 0.
+ */
+
+function checkNumber(num) {
+
+}
+console.log(checkNumber(5))
+// Output: Positif
+console.log(checkNumber(-3))
+// Output: Negatif
+console.log(checkNumber(0))
+// Output: Nol
+
+
+
+
+/**
+ * Buat sebuah program untuk menampilkan angka genap dari 1 hingga 20 menggunakan looping
+ */
